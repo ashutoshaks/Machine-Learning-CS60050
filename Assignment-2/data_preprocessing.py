ee4f846f00661e0766d1f252a191e8241370898e
ashutoshaks <ashu11.03.2002@gmail.com>
2021-09-29 01:23:56 +0530

    Add model

--- Assignment-2/data_preprocessing.py
+++ Assignment-2/data_preprocessing.py
@@ -1,3 +1,5 @@
+from metrics import accuracy
+from naive_bayes import NaiveBayes
 import pandas as pd
 import numpy as np
 import re
@@ -5,17 +7,22 @@ from nltk.corpus import stopwords
 
 import time
 
+stop_words = set(stopwords.words("english"))
+
+def tokenize(line: str):
+    tokens = re.findall("[a-z0-9]+", line.lower())
+    filtered_line = [word for word in tokens if not word in stop_words]
+    return filtered_line
+
+
 def process_data(file: str):
     df = pd.read_csv(file)
 
     text_list = []
     vocabulary = []
 
-    stop_words = set(stopwords.words("english"))
-
     for line in df['text']:
-        tokens = re.findall("[a-z0-9]+", line.lower())
-        filtered_line = [word for word in tokens if not word in stop_words]
+        filtered_line = tokenize(line)
         text_list.append(filtered_line)
         vocabulary.extend(filtered_line)
 
@@ -31,15 +38,17 @@ def process_data(file: str):
         for word in line:
             M[i][vocab_map[word]] = 1
 
-    author_map = {'EAP': 0,'HPL': 1,'MWS': 2}
+    # author_map = {'EAP': 0,'HPL': 1,'MWS': 2}
+    author_map = {author: i for i, author in enumerate(df['author'].unique())}
+    label_map = {i: author for author, i in author_map.items()}
     labels = df['author'].map(author_map).to_numpy()
 
-    return (M, labels, vocab_map, author_map)
+    return (M, labels, vocab_map, label_map)
 
 
 def train_test_split(features: np.ndarray, labels: np.ndarray, train_ratio: float = 0.7):
-    # idx = np.random.permutation(len(features))
-    # features, labels = features[idx], labels[idx]
+    idx = np.random.permutation(len(features))
+    features, labels = features[idx], labels[idx]
 
     split_index = int(train_ratio * len(features))
     X_train, y_train = features[:split_index], labels[:split_index]
@@ -51,19 +60,27 @@ def train_test_split(features: np.ndarray, labels: np.ndarray, train_ratio: floa
 if __name__ == '__main__':
     start = time.time()
 
-    M, labels, vocab_map, author_map = process_data('train.csv')
-    print(M.shape)
-    print(labels.shape)
-    print(len(vocab_map))
-    print(author_map)
+    M, labels, vocab_map, label_map = process_data('train.csv')
+    # print(M.shape)
+    # print(labels.shape)
+    # print(len(vocab_map))
+    # print(label_map)
+
+    X_train, y_train, X_test, y_test = train_test_split(M, labels)
+    # print(X_train.shape)
+    # print(y_train.shape)
+    # print(X_test.shape)
+    # print(y_test.shape)
+    
+    NB = NaiveBayes(alpha=1)
+    NB.fit(X_train, y_train, label_map)
+    print(NB.priors)
+    print(NB.likelihoods)
 
     print()
 
-    X_train, y_train, X_test, y_test = train_test_split(M, labels)
-    print(X_train.shape)
-    print(y_train.shape)
-    print(X_test.shape)
-    print(y_test.shape)
+    preds = NB.predict(X_test)
+    print(accuracy(y_test, preds))
 
     end = time.time()
     print(end - start)
---
+++ Assignment-2/naive_bayes.py
@@ -0,0 +1,54 @@
+from typing import Dict
+import numpy as np
+
+class NaiveBayes:
+
+    def __init__(self, alpha: int = 0):
+        self.alpha = alpha
+
+    def fit(self, X_train: np.ndarray, y_train: np.ndarray, label_map: Dict):
+        self.num_features = X_train.shape[1]
+        self.num_classes = len(label_map)
+        self.label_map = label_map
+        self.priors = np.empty(shape=self.num_classes)
+        self.likelihoods = np.empty(shape=(self.num_classes, self.num_features))
+
+        self.train(X_train, y_train, label_map)
+
+    def train(self, train_features: np.ndarray, labels: np.ndarray, label_map: Dict):
+        num_examples = labels.shape[0]
+        
+        for i in range(self.num_classes):
+            mask = labels == i
+            count_i = np.sum(mask)
+            print(count_i)
+            # calculate priors
+            self.priors[i] = count_i / num_examples
+
+            features_i = train_features[mask]
+            occ_cnt = np.sum(features_i, axis = 0)
+            print(occ_cnt)
+            print()
+
+            # calculate likelihoods
+            self.likelihoods[i] = (occ_cnt + self.alpha) / (features_i.shape[0] + self.alpha * self.num_features)
+
+
+    def predict_one(self, features: np.ndarray):
+        # probs = self.likelihoods * features
+        # temp = self.likelihoods[:, features == 1]
+        # for i in range(3):
+        #     print("zero = ", np.sum(temp[i] == 0))
+        probs = np.prod(self.likelihoods[:, features == 1], axis=1)
+        # print(probs.shape)
+        probs = probs * self.priors
+        # print(probs.shape)
+        # print(probs)
+        return np.argmax(probs)
+
+
+    def predict(self, test_features: np.ndarray):
+        preds = np.array([self.predict_one(feature) for feature in test_features])
+        # print(preds.shape)
+        # print(preds)
+        return preds
