a9406614063a5027473cadd0cd0e795312ae3589
ashutoshaks <ashu11.03.2002@gmail.com>
2021-10-19 12:36:37 +0530

    Change likelihood logic

--- Assignment-2/src/naive_bayes.py
+++ Assignment-2/src/naive_bayes.py
@@ -66,7 +66,8 @@ class NaiveBayes:
             occ_cnt = np.sum(features_i, axis = 0)
 
             # Calculate likelihoods
-            self.likelihoods[i] = (occ_cnt + self.alpha) / (features_i.shape[0] + self.alpha * self.num_features)
+            # self.likelihoods[i] = (occ_cnt + self.alpha) / (features_i.shape[0] + self.alpha * self.num_features)
+            self.likelihoods[i] = (occ_cnt + self.alpha) / (np.sum(occ_cnt) + self.alpha * self.num_features)
 
 
     def predict_one(self, features):
